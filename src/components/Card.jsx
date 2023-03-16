340d3136a089335c62dc68c7e17d48effbf3662e
Your Name <bozo.bakula@gmail.com>
2023-03-16 12:07:14 +0100

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const Card = ({ image, title, description, ordinalNumber }) => {
   return (
-    <div className="flex flex-col bg-white shadow-md h-auto sm:w-60 rounded-md cursor-pointer hover:scale-105 transition ease-in-out delay-150">
+    <div className="flex flex-col bg-white shadow-md h-auto rounded-md cursor-pointer hover:scale-105 transition ease-in-out delay-150">
       <img
         src={image}
         alt={title}
-        className="w-full h-[68px] sm:h-[300px] object-cover rounded-t-md"
+        className="w-full h-[114px] sm:h-[236px] xl:h-[250px] object-cover rounded-t-md"
       />
       <div className="flex flex-col justify-between items-start text-xs sm:text-sm p-1 sm:space-y-2">
         <h2>
           <span className="font-bold uppercase text-xs sm:text-sm">Title: </span>
           {title}
         </h2>
-        <p className="line-clamp-2 sm:line-clamp-4">
+        <p className="line-clamp-3 sm:line-clamp-4">
           <span className="font-bold uppercase text-xs sm:text-sm">Description: </span>
           {description}
         </p>
